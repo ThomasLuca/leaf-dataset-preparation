f34a7b2bd2763d85a8f49d3fae0429c985c8715b
Jens_Cocquyt <jenscocquyt@gmail.com>
2021-11-25 12:43:27 +0100

    fixed bug

--- ImageManipulation.m
+++ ImageManipulation.m
@@ -5,16 +5,18 @@ clear all
 % Open every image in the dataset
 files = dir('dataset/*.tif');
 for file = files'
-    fileName = file.name;
+    fileName = file.name
     textIm = imread(strcat(file.folder, '/', file.name));
 
     % find threshold and change to binary image
     border = graythresh(textIm);
     textbw = ~im2bw(textIm, border);
     
+    
     % remove noise with median filter
     textfilt = medfilt2(textbw,[7 7]);
-    textfilt = bwareaopen(textfilt,8);
+    textfilt = bwareaopen(textfilt,4);
+
     
     % Use an absurdely large line structuring element oriented at 25 degrees
     % to make the a's stand out
@@ -22,10 +24,12 @@ for file = files'
     se = strel('line', 20 ,25);
     textfilt = imclose(textfilt, se);
     
+    
     % Get a couple properties. Note the "Eccentricity"
     S = regionprops(textfilt, 'Area','Eccentricity','Centroid','BoundingBox');
-    
+
     All_areas = vertcat(S.Area);
+
     
     % Find the largest element (i.e. the big a). We will use it to get its
     % eccentricity and fetch other a's.
@@ -33,28 +37,27 @@ for file = files'
     [MaxArea, MaxAreaIdx] = (max(All_areas(:)));
     
     % Get eccentricity of largest letter.
-    RefEcc = S(MaxAreaIdx).Eccentricity
+    RefEcc = S(MaxAreaIdx).Eccentricity;
     
     % Just concatenate everything. Easier to work with.
     All_Ecc = vertcat(S.Eccentricity);
     All_Centroids = vertcat(S.Centroid);
-    All_BB = vertcat(S.BoundingBox)
+    All_BB = vertcat(S.BoundingBox);
     
     % Find elements that have the approximate eccentricity of the large a
     % found earlier. You can be more/less stringent and add more conditions here.
     
-    PotA = find(All_Ecc > RefEcc*.8 & All_Ecc < RefEcc*1.2)
+    PotA = find(All_Ecc > RefEcc*.8 & All_Ecc < RefEcc*1.2);
     
-    scatter(All_Centroids(PotA,1),All_Centroids(PotA,2),60,'r','filled');
+    %scatter(All_Centroids(PotA,1),All_Centroids(PotA,2),60,'r','filled');
     
-    for k = 1:numel(PotA)
-        Content = All_BB(PotA(k),:);
-    end
+    Content = All_BB(PotA(1),:);
     
     %Convert to grayscale
     textIm = rgb2gray(textIm);
-    cropped = imcrop(textIm, Content);
     
+    cropped = imcrop(textIm, Content);
+
     [lengthY, lengthX] = size(cropped);
     
     % Make the leaf extractions square by adding a margin to the top and
@@ -82,9 +85,8 @@ for file = files'
     end
     
     % Resize to a 50x50 pixel image
-    resized = imresize(cropped,[50 50]);
+    resized = imresize(cropped,[324 324]);
     
     % Save image
     imwrite(resized, strcat('prepared/',fileName));
 end
-
